4ad6fb762bf0384f4bff8957f55476d4a4ac2b3a
mingquan_liu@163.com <lmq5083468163>
2018-12-13 20:39:47 +0800

    Initial commit

---
+++ src/main/java/com/sosbuger/app/Test01.java
@@ -0,0 +1,29 @@
+package com.sosbuger.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Test01 {
+
+
+    public static void main(String[] args) {
+        System.setProperty("my.log","systemLog");
+        Logger logger = LoggerFactory.getLogger(Test01.class);
+        for (int i = 0; i < 200; i++) {
+            if(i % 2 == 0){
+                logger.info("info: "+i);
+            }else{
+                logger.debug("debug: "+ i);
+            }
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+}
